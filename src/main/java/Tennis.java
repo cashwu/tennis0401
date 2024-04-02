cac1f8906d379afba532a9e6024fc7e707b50342
Cash <cashwugeek@icloud.com>
2024-04-02 14:52:23 +0800

    tennis

--- src/main/java/Tennis.java
+++ src/main/java/Tennis.java
@@ -1,14 +1,40 @@
+import java.util.HashMap;
+
 public class Tennis {
-    private int firstPlayerTimes;
+    private final HashMap<Integer, String> scoreLookup = new HashMap<Integer, String>() {{
+        put(0, "love");
+        put(1, "fifteen");
+        put(2, "thirty");
+        put(3, "forty");
+    }};
+    private int firstPlayerScoreTimes;
+    private int secondPlayerScoreTimes;
 
     public String score() {
-        if (firstPlayerTimes == 1) {
-            return "fifteen love";
+        if (isScoreDifferent()) {
+            return lookupScore();
         }
-        return "love all";
+
+        return sameScore();
+    }
+
+    private boolean isScoreDifferent() {
+        return secondPlayerScoreTimes != firstPlayerScoreTimes;
+    }
+
+    private String lookupScore() {
+        return String.format("%s %s", scoreLookup.get(firstPlayerScoreTimes), scoreLookup.get(secondPlayerScoreTimes));
+    }
+
+    private String sameScore() {
+        return scoreLookup.get(firstPlayerScoreTimes) + " all";
     }
 
     public void firstPlayScore() {
-        firstPlayerTimes++;
+        firstPlayerScoreTimes++;
+    }
+
+    public void secondPlayerScore() {
+        secondPlayerScoreTimes++;
     }
 }
--- src/test/java/TennisTests.java
+++ src/test/java/TennisTests.java
@@ -10,16 +10,73 @@ public class TennisTests {
     @Test
     public void love_all() {
 
-
         scoreShouldBe("love all");
     }
 
     @Test
     public void fifteen_love() {
 
-
-        tennis.firstPlayScore();
+        givenFirstPlayScore(1);
         scoreShouldBe("fifteen love");
+    }
+
+    @Test
+    public void thirty_love() {
+
+        givenFirstPlayScore(2);
+        scoreShouldBe("thirty love");
+    }
+
+    @Test
+    public void forty_love() {
+
+        givenFirstPlayScore(3);
+        scoreShouldBe("forty love");
+    }
+
+    @Test
+    public void love_fifteen() {
+
+        giveSecondPlayerScore(1);
+        scoreShouldBe("love fifteen");
+    }
+
+    @Test
+    public void love_thirty() {
+
+        giveSecondPlayerScore(2);
+        scoreShouldBe("love thirty");
+    }
+
+    @Test
+    public void fifteen_all() {
+
+        giveSecondPlayerScore(1);
+        givenFirstPlayScore(1);
+        scoreShouldBe("fifteen all");
+    }
+
+    @Test
+    public void thirty_all() {
+
+        giveSecondPlayerScore(2);
+        givenFirstPlayScore(2);
+        scoreShouldBe("thirty all");
+    }
+
+    private void giveSecondPlayerScore(int times) {
+        for (int i = 0; i < times; i++) {
+
+            tennis.secondPlayerScore();
+        }
+
+    }
+
+    private void givenFirstPlayScore(int times) {
+        for (int i = 0; i < times; i++) {
+
+            tennis.firstPlayScore();
+        }
     }
 
     private void scoreShouldBe(String expected) {
